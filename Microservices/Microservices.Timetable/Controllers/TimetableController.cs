51cc6c20221ca2353615bc9f04b39dd740cce9e1
Joseph <ostafeiosif_2010@yahoo.com>
2017-05-02 19:49:11 +0300

    Add Controller for Microservices.Timetable.

---
+++ Microservices/Microservices.Timetable/Controllers/TimetableController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Net;
+using System.Web.Http;
+using System.Web.Script.Serialization;
+using Microservices.Timetable.Services;
+
+namespace Microservices.Timetable.Controllers
+{
+    public class TimetableController : ApiController
+    {
+        private readonly TimetableService _timetableService;
+
+        public TimetableController()
+        {
+            _timetableService = new TimetableService();
+        }
+
+        [Route("api/v1/timetables")]
+        [HttpGet]
+        public IHttpActionResult Get()
+        {
+            try
+            {
+                var timetables = _timetableService.GetTimeTables();
+                var result = new JavaScriptSerializer().Serialize(timetables);
+                return this.JsonString(result);
+            }
+            catch (Exception)
+            {
+                var message = "";
+                return this.JsonString(message, HttpStatusCode.BadRequest);
+            }
+        }
+
+        [Route("api/v1/timetable/{id}")]
+        [HttpGet]
+        public IHttpActionResult Get(int id)
+        {
+            try
+            {
+                var timetable = _timetableService.GetById(id);
+                var result = new JavaScriptSerializer().Serialize(timetable);
+                return this.JsonString(result);
+            }
+            catch (Exception)
+            {
+                var message = "";
+                return this.JsonString(message, HttpStatusCode.BadRequest);
+            }
+        }
+    }
+}
