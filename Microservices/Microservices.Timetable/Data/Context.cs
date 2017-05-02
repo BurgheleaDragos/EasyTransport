a0fb4c3fa223f389b89504688fd796535eaee4f0
Joseph <ostafeiosif_2010@yahoo.com>
2017-05-02 19:48:43 +0300

    Add Context.

---
+++ Microservices/Microservices.Timetable/Data/Context.cs
@@ -0,0 +1,24 @@
+﻿using System.Data.Entity;
+using Microservices.Timetable.Data.Entities;
+using Microservices.Timetable.Data.Mapping;
+
+namespace Microservices.Timetable.Data
+{
+    public class Context : DbContext
+    {
+        public Context() : base("Microservices.TimeTable")
+        {
+        }
+
+        public DbSet<Place> Places { get; set; }
+        public DbSet<TimeTable> TimeTables { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Configurations.Add(new PlaceMappingConfiguration());
+            modelBuilder.Configurations.Add(new TimeTableMappingConfiguration());
+        }
+    }
+}
