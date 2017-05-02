600e8265bd3f53714a2c12c8903e2ea562d445fc
Joseph <ostafeiosif_2010@yahoo.com>
2017-05-02 19:50:11 +0300

    Add service for database CRUD operations.

---
+++ Microservices/Microservices.Timetable/Services/TimeTableService.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Microservices.Timetable.Data;
+using Microservices.Timetable.Data.Entities;
+
+namespace Microservices.Timetable.Services
+{
+    public class TimetableService
+    {
+        public List<TimeTable> GetTimeTables()
+        {
+            var context = new Context();
+            var result = context.TimeTables.ToList();
+            return result;
+        }
+
+        public TimeTable GetById(int id)
+        {
+            var context = new Context();
+            var result = context.TimeTables.FirstOrDefault(t => t.Id == id);
+            return result;
+        }
+
+        public List<TimeTable> GetByRoute(string origin, string destination)
+        {
+            return null;
+        }
+
+        public List<TimeTable> GetByTime(long departure, long arrival)
+        {
+            return null;
+        }
+    }
+}
