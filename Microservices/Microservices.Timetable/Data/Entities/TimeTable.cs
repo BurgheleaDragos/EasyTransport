3c3bbd1f144bf405b04726046947a601cf161963
Joseph <ostafeiosif_2010@yahoo.com>
2017-05-02 19:47:40 +0300

    Add Place Entity for DbContext.

---
+++ Microservices/Microservices.Timetable/Data/Entities/TimeTable.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Microservices.Timetable.Data.Entities
+{
+    public class TimeTable
+    {
+        public int Id { get; set; }
+        public TimeSpan DepartureTime { get; set; }
+        public TimeSpan ArrivalTime { get; set; }
+        public double TravelPrice { get; set; }
+        public string Details { get; set; }
+
+        public virtual Place OriginPlace { get; set; }
+        public virtual Place DestinationPlace { get; set; }
+    }
+}
