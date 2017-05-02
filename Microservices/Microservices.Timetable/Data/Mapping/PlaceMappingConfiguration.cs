481b35a08c31f1f80a95ee0db1cc8c5e7dd2ee02
Joseph <ostafeiosif_2010@yahoo.com>
2017-05-02 19:48:24 +0300

    Add MappingConfiguration for DbModelBuilder.

---
+++ Microservices/Microservices.Timetable/Data/Mapping/PlaceMappingConfiguration.cs
@@ -0,0 +1,18 @@
+﻿using System.Data.Entity.ModelConfiguration;
+using Microservices.Timetable.Data.Entities;
+
+namespace Microservices.Timetable.Data.Mapping
+{
+    public class PlaceMappingConfiguration : EntityTypeConfiguration<Place>
+    {
+        public PlaceMappingConfiguration()
+        {
+            HasKey(t => t.Id);
+
+            Property(t => t.City).IsRequired().HasMaxLength(32);
+            Property(t => t.Name).IsRequired().HasMaxLength(64);
+            Property(t => t.Address).IsRequired().HasMaxLength(128);
+            Property(t => t.Details).IsOptional().HasMaxLength(1024);
+        }
+    }
+}
---
+++ Microservices/Microservices.Timetable/Data/Mapping/TimeTableMappingConfiguration.cs
@@ -0,0 +1,18 @@
+﻿using System.Data.Entity.ModelConfiguration;
+using Microservices.Timetable.Data.Entities;
+
+namespace Microservices.Timetable.Data.Mapping
+{
+    public class TimeTableMappingConfiguration : EntityTypeConfiguration<TimeTable>
+    {
+        public TimeTableMappingConfiguration()
+        {
+            HasKey(t => t.Id);
+
+            Property(t => t.TravelPrice).IsRequired();
+            Property(t => t.ArrivalTime).IsRequired();
+            Property(t => t.DepartureTime).IsRequired();
+            Property(t => t.Details).IsOptional().HasMaxLength(1024);
+        }
+    }
+}
